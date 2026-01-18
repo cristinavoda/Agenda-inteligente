5f10f9bc819fd58f32d1d93de3f53381c1752a25
CristinaVoda <vodacristina7@gmail.com>
2026-01-18 10:54:49 +0100

    PA11

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue()],
+ plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: false
+    })
+  ],
+
   server: {
     host: true,     
     port: 5501,
