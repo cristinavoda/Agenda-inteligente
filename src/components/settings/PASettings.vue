6a4a7c9de1c1cf74e03b0abf7859775a509b9a06
CristinaVoda <cristinavoda483@gmail.com>
2026-08-27 18:46:23 +0200

    add 'new content 16'

--- src/components/settings/PASettings.vue
+++ src/components/settings/PASettings.vue
@@ -11,7 +11,8 @@
 .settings-card{
     padding:1.5rem;
     border:1px solid #d8d8d8;
-    border-radius:16px;
+    border-radius:0px;
     background:#fafafa;
+    color: rgb(95, 94, 86);
 }
 </style>
